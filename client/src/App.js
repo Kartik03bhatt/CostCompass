9f5cf29fb74734987acd5f9a05b245728b59a676
Kartik03bhatt <bhattkartik1500@gmail.com>
2023-04-21 15:52:33 +0530

    frontend_final

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Form from './Components/Form';
 function App() {
   return (
     <div className="App">
-      <div className="container mx-auto max-w-xl  text-center drop-shadow-lg text-gray-800 ">
+      <div className="container mx-auto max-w-7xl  text-center drop-shadow-lg text-gray-800 ">
       <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">Expense Tracker</h1>
       {/* grid coloumns */}
       <div className="grid md:grid-cols-2 gap-4">
